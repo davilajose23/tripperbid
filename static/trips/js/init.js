32a2afedf6b263fb554b029b66968275a096ff98
Jose Fernando <davila.jose23@gmail.com>
2015-12-26 17:12:30 -0600

    changed init.js

--- static/trips/js/init.js
+++ static/trips/js/init.js
@@ -7,35 +7,26 @@
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
 
-
     $('.parallax').parallax();
     $('select').material_select();
 
-    $(document).ready(function(){
-
-
     var options = [
     {selector: '#staggered-test', offset: 10, callback: 'Materialize.showStaggeredList("#staggered-test")' },
-    {selector: '#image-test', offset: -1, callback: 'Materialize.fadeInImage("#image-test")' } ];
+    {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' } ];
 
     Materialize.scrollFire(options);
 
-
-
   }); // end of document ready
-
-
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
+    selectYears: 80 // Creates a dropdown of 15 years to control year
 
+  });
   $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
-    $('.tabs-wrapper ').pushpin({ top: $('.tabs-wrapper').offset().top });
-
     $('.tooltipped').tooltip({delay: 50});
+
 
   });
 })(jQuery); // end of jQuery name space
